afcea1876b1de758ad4f5e8bd1f3ac06e6ca3ffb
Simeon Pande <simeonpande@outlook.com>
2019-07-30 11:41:55 +0300

    Changes on node file

---
+++ src/node/energy.py
@@ -0,0 +1,15 @@
+
+
+class EnergyGenerator(object):
+    def __init__(self, powertype):
+
+
+class Energy(object):
+    def __init__(self, adc, tranceiver, processor, storage):
+        self.adc = adc
+        self.tranceiver = tranceiver
+        self.processor = processor
+        self.storage = storage
+
+
+
